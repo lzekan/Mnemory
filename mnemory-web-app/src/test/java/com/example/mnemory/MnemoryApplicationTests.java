9451ee0a4ebd1742b06c4a00cdfbea44597b2376
Luka Zekan <luka.zekan983@gmail.com>
2023-03-30 18:16:15 +0200

    inital commit of rest api and android app

---
+++ mnemory-web-app/src/test/java/com/example/mnemory/MnemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mnemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
