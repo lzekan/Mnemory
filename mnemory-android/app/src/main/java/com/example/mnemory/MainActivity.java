473eaf96e7f474ba0678e4b66b2a6d7103ba3db9
Luka Zekan <lz53052@fer.hr>
2023-06-01 12:07:01 +0200

    maketextovi u slucaju krivog/nepostojeceg unosa podataka login/register

--- mnemory-android/app/src/main/java/com/example/mnemory/MainActivity.java
+++ mnemory-android/app/src/main/java/com/example/mnemory/MainActivity.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +34,12 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                EditText inputUsername = (EditText) findViewById(R.id.inputUsername);
+                EditText inputPassword = (EditText) findViewById(R.id.inputPassword);
 
+                if(inputUsername.getText().length() == 0 || inputPassword.getText().length() == 0){
+                    Toast.makeText(getApplicationContext(), "Molimo popunite prazna polja.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- mnemory-android/app/src/main/java/com/example/mnemory/RegisterActivity.java
+++ mnemory-android/app/src/main/java/com/example/mnemory/RegisterActivity.java
@@ -1,13 +1,28 @@
 package com.example.mnemory;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.matches();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +30,37 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         TextView linkToLogin = findViewById(R.id.linkToLogin);
+        Button btnRegister = findViewById(R.id.btnRegister);
 
-        linkToLogin.setOnClickListener(new View.OnClickListener() {
+
+
+        linkToLogin.setOnClickListener(v -> startActivity(new Intent(RegisterActivity.this, MainActivity.class)));
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                EditText inputUsername = (EditText) findViewById(R.id.inputUsername);
+                EditText inputEmail = (EditText) findViewById(R.id.inputEmail);
+                EditText inputPassword = (EditText) findViewById(R.id.inputPassword);
+
+                if(inputUsername.getText().length() == 0 || inputEmail.getText().length() == 0){
+                    Toast.makeText(getApplicationContext(), "Molimo popunite prazna polja.", Toast.LENGTH_SHORT).show();
+                }
+
+                if(inputPassword.getText().length() < 6){
+                    Toast.makeText(getApplicationContext(), "Lozinka mora imati bar 6 znakova.", Toast.LENGTH_SHORT).show();
+                }
+
+                if(!validate(String.valueOf(inputEmail.getText()))){
+                    Toast.makeText(getApplicationContext(), "Molimo unesite pravilnu email adresu.", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+
+
+
+
 
 
     }
